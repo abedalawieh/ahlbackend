5750140968770caff1157a226c2aa4b3eaa1132f
Abed Alawieh <124059093+abedalawieh@users.noreply.github.com>
2024-05-27 13:13:12 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import mysql from "mysql";
 const db = mysql.createConnection({
-    host:"sql6.freemysqlhosting.net",
-    user : "sql6640921",
-    password: "PQTyEQShDy",
-    database: "sql6640921"
+    host:"sql12.freemysqlhosting.net",
+    user : "sql12709634",
+    password: "ttjXRKaRii",
+    database: "sql12709634"
 })
 db.connect((error)=>{
     if(error) throw error
